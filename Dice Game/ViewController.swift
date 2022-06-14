cf1cc1d6b38e6cf4ac0e273ec004d3236b14e601
Vikas Saini <vikassaini883@gmail.com>
2022-06-13 20:13:12 -0400

    Initial Commit

---
+++ Dice Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Game
+//
+//  Created by Vikas Saini on 2022-06-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
